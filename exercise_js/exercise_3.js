5e36125e1d787db5572d4e2dbed36db07132f42c
st-linhdoan <linh.doan@supremetech.vn>
2021-04-26 15:49:56 +0700

    create exercise_3 and answer excercise_3

---
+++ exercise_js/exercise_3.js
@@ -0,0 +1,9 @@
+function  findNumber(str) {
+  var arr = [];
+  for(var i = 0; i < 10 ; i++){
+    if(Number(str.replace("*", i))%3 == 0){
+        arr.push(str.replace("*",i))
+    }
+  }
+  return arr;
+}
