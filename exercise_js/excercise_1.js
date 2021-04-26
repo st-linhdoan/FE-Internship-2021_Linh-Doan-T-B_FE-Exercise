04f0cef9658bd4083c602b94bddfc4da304622a5
st-linhdoan <linh.doan@supremetech.vn>
2021-04-26 15:20:53 +0700

    create file and answer exercise_1

---
+++ exercise_js/excercise_1.js
@@ -0,0 +1,12 @@
+
+function sumNumber(a,b){
+    var sum = 0;
+    if(a == b){
+        sum = (a+b)*3
+    }
+    else{
+        sum = a + b; 
+    }
+    return sum;
+}
+console.log(sumNumber(7,5));
