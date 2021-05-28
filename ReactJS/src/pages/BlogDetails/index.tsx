76fdd69389d2e84f0e1ce059867708aa3a08e910
st-linhdoan <linh.doan@supremetech.vn>
2021-05-28 15:08:44 +0700

    add feature is loading

--- ReactJS/src/pages/BlogDetails/index.tsx
+++ ReactJS/src/pages/BlogDetails/index.tsx
@@ -7,19 +7,30 @@ import { IDetail } from '../../interface/IPost';
 const BlogDetails: React.FC<IDetail> = () => {
   const {id} = useParams();
   const [detailBlog, setDetailBlog] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     axios.get(API.API_ARTICLE + `/${id}`)
       .then(function (res) {
-        const data = res.data;
-        setDetailBlog(data);
+        if(res && res.status === 200){
+          const data = res.data;
+          setDetailBlog(data);
+          setIsLoading(true);
+        }
       })
       .catch(function (err) {
         console.log(err);
       })
-    
   }, [])
   return (
-    <BlogDetail detail = {detailBlog}/>
+    <>
+    {
+      isLoading ?<> <BlogDetail detail={detailBlog} /> {console.log(isLoading)} </>
+      : <div className="img-loading">
+          {console.log("img")}
+          <img src="https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" />
+        </div>
+    }
+    </>
   );
 }
 
--- ReactJS/src/pages/Blogs/index.tsx
+++ ReactJS/src/pages/Blogs/index.tsx
@@ -8,11 +8,15 @@ import './blogs.scss';
 
 const Blogs: React.FC<IPost> = () => {
   const [listPost, setListPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     axios.get(API.API_ARTICLE)
       .then(function (res) {
-        const data = res.data;
-        setListPost(data);
+        if(res && res.status === 200){
+          const data = res.data;
+          setListPost(data);
+          setIsLoading(true);
+        }
       })
       .catch(function (err) {
         console.log(err);
@@ -22,19 +26,26 @@ const Blogs: React.FC<IPost> = () => {
   return (
     <>
     <div className="container">
-      <ul className="posts">
-        {
-          listPost.map((item: IPost) => {
-            return (
-              <li key={item.id.toString()} className="post-item">
-                <Link to={`/articles/${item.id}`}>
-                  <Blog post={item} />
-                </Link>
-              </li>
-            )
-          })
-        }
-      </ul>
+      {
+      isLoading ? 
+        <ul className="posts">
+          {
+            listPost.map((item: IPost) => {
+              return (
+                <li key={item.id.toString()} className="post-item">
+                  <Link to={`/articles/${item.id}`}>
+                    <Blog post={item} />
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      : 
+        <div className="img-loading">
+          <img src= "https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" />
+        </div>
+      }
     </div>
     </>
   );
